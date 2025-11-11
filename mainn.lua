6069108c67ca2c53600b9637f4cca550a33260f8
WonyeScripts <alikeremkanbur74@icloud.com>
2025-11-11 14:16:45 +0300

    Delete mainn.lua

--- mainn.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/alikeremkanburr/WonyeHub/refs/heads/main/multi-game-support.lua", true))()
-task.wait(0.5)
-loadstring(game:HttpGet("https://pastefy.app/2Q3qbI5q/raw"))()
